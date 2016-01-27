5c2be5ddc0baafae749eba30ce19a2c6deebcc0d
chubachi <chubachi@curicle.jp>
2016-01-27 23:22:14 +0900

    add rainbow-delimiters setting and show-paren-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -78,3 +78,18 @@
     '(progn
        (add-to-list 'ac-modes 'cider-mode)
        (add-to-list 'ac-modes 'cider-repl-mode)))
+
+;; rainbow-delimiters
+(show-paren-mode t)
+(require 'rainbow-delimiters)
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+(require 'cl-lib)
+(require 'color)
+(defun rainbow-delimiters-using-stronger-colors ()
+  (interactive)
+  (cl-loop
+   for index from 1 to rainbow-delimiters-max-face-count
+   do
+   (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
+    (cl-callf color-saturate-name (face-foreground face) 30))))
+(add-hook 'emacs-startup-hook 'rainbow-delimiters-using-stronger-colors)
