ee71ce5db5492b8da7a40d02a0c8e67cb7cef247
cyubachi <cyubachi@gmail.com>
2016-04-13 19:33:53 +0900

    disable workgroups module and add comment key bind

--- .emacs.d/init.d/05_workgroups_settings.el
+++ .emacs.d/init.d/05_workgroups_settings.el
@@ -1,34 +1,34 @@
-(add-to-list 'load-path "~/workgroups/workgroups.el")
-(setq wg-prefix-key (kbd "C-c w"))
-(require 'workgroups)
-(wg-load "~/workgroups/wg01")
-(add-hook 'auto-save-hook
-          (lambda ()
-            (wg-update-all-workgroups)
-            (wg-save "~/workgroups/wg01")))
-(add-hook 'kill-emacs-hook
-          (lambda ()
-            (wg-update-all-workgroups)
-            (wg-save "~/workgroups/wg01")))
+;; (add-to-list 'load-path "~/workgroups/workgroups.el")
+;; (setq wg-prefix-key (kbd "C-c w"))
+;; (require 'workgroups)
+;; (wg-load "~/workgroups/wg01")
+;; (add-hook 'auto-save-hook
+;;           (lambda ()
+;;             (wg-update-all-workgroups)
+;;             (wg-save "~/workgroups/wg01")))
+;; (add-hook 'kill-emacs-hook
+;;           (lambda ()
+;;             (wg-update-all-workgroups)
+;;             (wg-save "~/workgroups/wg01")))
 
 
-(workgroups-mode 1)
-(require 'switch-window)
-(select-window (third (switch-window--list)))
+;; (workgroups-mode 1)
+;; (require 'switch-window)
+;; (select-window (third (switch-window--list)))
 
-(defun display-main-window (buffer alist)
-  (window--display-buffer buffer (second (switch-window--list)) 'reuse))
-(defun display-third-window (buffer alist)
-  (select-window (nth 2 (switch-window--list)))
-  (window--display-buffer buffer (nth 2 (switch-window--list)) 'reuse))
-(defun display-forth-window (buffer alist)
-  (select-window (nth 3 (switch-window--list)))
-  (window--display-buffer buffer (nth 3 (switch-window--list)) 'reuse))
+;; (defun display-main-window (buffer alist)
+;;   (window--display-buffer buffer (second (switch-window--list)) 'reuse))
+;; (defun display-third-window (buffer alist)
+;;   (select-window (nth 2 (switch-window--list)))
+;;   (window--display-buffer buffer (nth 2 (switch-window--list)) 'reuse))
+;; (defun display-forth-window (buffer alist)
+;;   (select-window (nth 3 (switch-window--list)))
+;;   (window--display-buffer buffer (nth 3 (switch-window--list)) 'reuse))
 
-(setq inhibit-startup-screen t)
-(setq display-buffer-alist
-      '(("*anything.*?*" . (display-forth-window . nil))
-        ("*cider.*?*" . (display-forth-window . nil))
-        ("*eshell.*?*" . (display-third-window . nil))))
+;; (setq inhibit-startup-screen t)
+;; (setq display-buffer-alist
+;;       '(("*anything.*?*" . (display-forth-window . nil))
+;;         ("*cider.*?*" . (display-forth-window . nil))
+;;         ("*eshell.*?*" . (display-third-window . nil))))
 
-(eshell)
+;; (eshell)
