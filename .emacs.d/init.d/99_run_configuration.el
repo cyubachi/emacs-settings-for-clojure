8aee1d764c056b1caf66b77d0a1be24dffa8ebd8
chubachi <chubachi@curicle.jp>
2016-09-19 11:58:35 +0900

    enable to run "run configuration" on windows.

--- .emacs.d/init.d/99_run_configuration.el
+++ .emacs.d/init.d/99_run_configuration.el
@@ -1,27 +1,34 @@
+;;; 99_run_configuration -- run configuration settings.
+
+(defvar command-delimiter ";")
+(if (eq system-type 'windows-nt)
+    (setq command-delimiter "&"))
+
 (defun exec-lein-ring-server-prod ()
   (interactive)
-  (async-shell-command (format "cd %s; lein with-profile prod ring server" ffip-project-root)))
+  (async-shell-command (format "cd %s %s lein with-profile prod ring server" ffip-project-root command-delimiter)))
 
 (defun exec-lein-ring-server ()
   (interactive)
-  (async-shell-command (format "cd %s; lein ring server" ffip-project-root)))
+  (async-shell-command (format "cd %s %s lein ring server" ffip-project-root command-delimiter)))
 
 (defun exec-lein-deps ()
   (interactive)
-  (async-shell-command (format "cd %s; lein deps" ffip-project-root)))
+  (message ffip-project-root)
+  (async-shell-command (format "cd %s %s lein deps" ffip-project-root command-delimiter)))
 
 (defun exec-lein-cljsbuild ()
   (interactive)
-  (async-shell-command (format "cd %s; lein cljsbuild auto" ffip-project-root)))
+  (async-shell-command (format "cd %s %s lein cljsbuild auto" ffip-project-root command-delimiter)))
 
 (defun exec-lein-figwheel ()
   (interactive)
-  (async-shell-command (format "cd %s; lein figwheel" ffip-project-root))
+  (async-shell-command (format "cd %s %s lein figwheel" ffip-project-root command-delimiter))
   (switch-to-buffer (buffer-name (find-file-noselect (concat ffip-project-root "/figwheel_server.log")))))
 
 (defun exec-lein-compile ()
   (interactive)
-  (async-shell-command (format "cd %s; lein clean; lein compile" ffip-project-root)))
+  (async-shell-command (format "cd %s %s lein clean; lein compile" ffip-project-root command-delimiter)))
 
 ;; when a .log file is opened, apply to auto-revert-tail-mode
 (add-to-list 'auto-mode-alist '("\\.log$" . auto-revert-tail-mode))
