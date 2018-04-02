00de97fc8945b8b6e707c8ac2d333dcd280a4743
cyubachi <cyubachi@gmail.com>
2018-04-03 00:10:08 +0900

    modify magit window.

--- .emacs.d/init.d/02_magit_settings.el
+++ .emacs.d/init.d/02_magit_settings.el
@@ -1,2 +1,6 @@
 ;; magit-config
 (global-set-key (kbd "C-x g") 'magit-status)
+
+(setq magit-display-buffer-function
+      (lambda (buffer)
+        (display-buffer buffer '(display-buffer-same-window))))
